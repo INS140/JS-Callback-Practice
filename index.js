dbefcd72a6476132ea939603964eda639844fc58
INS140 <IsaacNSaettel@gmail.com>
2022-12-14 00:36:09 -0800

    cleanup and tree line

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+let objectArray = [] //used for character z index positioning
+
+const createTreeLine = () => {
+    for (let i = 0; i*50 < window.innerWidth; i++) {
+        let tree = newImage('assets/pine-tree.png')
+        tree.style.zIndex = '0'
+        move(tree).to((i*50)-50, (window.innerHeight-(window.innerHeight*16/100)))
+        objectArray.pop()
+    }
+}
+
+createTreeLine()
+
 const updateCharacterDirection = (direction) => {
     if (direction === null) {
         character.src = 'assets/green-character/static.gif'
@@ -16,8 +29,6 @@ const updateCharacterDirection = (direction) => {
     }
 }
 
-let objectArray = [] //used for character z index positioning
-
 createBackground('assets/sky.png', '14vh') // must be created first
 createBackground('assets/grass.png', '86vh')
 const inventory = newInventory()
@@ -26,25 +37,16 @@ move(inventory).to(0, 0)
 const character = newImage('assets/green-character/static.gif')
 objectArray.pop()
 
-const tree = newImage('assets/tree.png')
-const pillar = newImage('assets/pillar.png')
-const pineTree = newImage('assets/pine-tree.png')
-const crate = newImage('assets/crate.png')
-const well = newImage('assets/well.png')
-const sword = newItem('assets/sword.png')
-const shield = newItem('assets/shield.png')
-const staff = newItem('assets/staff.png')
+move(character).withArrowKeys(100, 250, updateCharacterDirection)
 
-move(character).withArrowKeys(150, 351, updateCharacterDirection)
-
-move(tree).to(200, 450)
-move(pillar).to(350, 250)
-move(pineTree).to(450, 350)
-move(crate).to(150, 350)
-move(well).to(500, 575)
-move(sword).to(500, 555)
-move(shield).to(165, 335)
-move(staff).to(600, 250)
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
 
 const updateCharacterZIndex = () => { // Sets the characters z index based off of objects around
     let charBtm = Number(character.style.bottom.replace(/px/g, ''))
